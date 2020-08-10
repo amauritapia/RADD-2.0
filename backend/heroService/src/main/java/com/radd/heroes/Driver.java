332c23414d2eade56cc99277404bda072a6965df
Rennie-Wallin <wallinrennie@gmail.com>
2020-08-10 12:45:42 -0700

    adding a test for zuul

---
+++ backend/heroService/src/main/java/com/radd/heroes/Driver.java
@@ -0,0 +1,16 @@
+package com.radd.heroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Driver 
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Driver.class, args);
+	}
+
+}
