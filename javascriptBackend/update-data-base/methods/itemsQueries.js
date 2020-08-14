970dd528a1563d2fc72b5403f34b5b53d7f8dde6
amauritapia <amauri.tapia@gmail.com>
2020-08-14 14:03:26 -0700

    making some progress on parsing

--- javascriptBackend/update-data-base/methods/itemsQueries.js
+++ javascriptBackend/update-data-base/methods/itemsQueries.js
@@ -4,8 +4,9 @@ const fs = require("fs");
 async function getItems(){
     return await axios.get("https://api.pandascore.co/dota2/items/?range[id]=1,100&per_page=100&token=nPbfyNrliDhjXrJisutMh5Dw5oYctBTHStEtLIN_UrlDC9-id_I")
     .then(function (response){
-        data = JSON.parse(response.data);
-        fs.writeFile('/home/amauri-tapia/Desktop/test.csv', data,(err) =>{
+        data = response.data;
+        console.log(data[99].name)
+        fs.writeFile('/home/amauri-tapia/Desktop/test.csv', data.id,(err) =>{
             if(err) throw err;
         })
     }).catch(function (error){
