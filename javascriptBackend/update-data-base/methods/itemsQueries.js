2df99ccdd81a218f7e236a1145949f3f65cea828
amauritapia <amauri.tapia@gmail.com>
2020-08-17 12:22:02 -0700

    working on refining the axios request for items with straz api

--- javascriptBackend/update-data-base/methods/itemsQueries.js
+++ javascriptBackend/update-data-base/methods/itemsQueries.js
@@ -2,14 +2,36 @@ const axios = require("axios");
 const fs = require("fs");
 
 async function getItems(){
-    return await axios.get("https://api.opendota.com/api/scenarios/itemTimings")
+    return await axios.get("https://api.stratz.com/api/v1/Item")
     .then(function (response){
         data = response.data;
-        //console.log(data);
-        for(i=0; i< data.length;i++){
-        fs.appendFile('/home/amauri-tapia/Desktop/test.csv',data[i].hero_id + "," + data[i].item + "\r\n",(err) =>{
-            if(err) throw err;
-        })}
+        /*for(n = 1; n < data.length; n++){
+            if(data[n] === undefined){
+                console.log('This is not defined. Skipping')
+            }
+            else{
+                console.log(n,data[n].language['displayName'])
+            }
+        }*/
+
+        for(i=1; i < 1032;i++){
+            if(data[i] === undefined){
+                fs.appendFile('/home/amauri-tapia/Desktop/test-2.csv',"",(err) =>{
+                    if(err) throw err;
+                })
+            } else{
+                if(data[i].language['description'] == ""){
+                    fs.appendFileSync('/home/amauri-tapia/Desktop/test-2.csv',data[i].id + "," + data[i].name + "," + "N/A" + "\r\n",(err) =>{
+                        if(err) throw err;
+                        })
+                }
+                else{
+                fs.appendFileSync('/home/amauri-tapia/Desktop/test-2.csv',data[i].id + "," + data[i].name + "," + data[i].language['description']+ "\r\n",(err) =>{
+                    if(err) throw err;
+                    })
+                }
+            }
+        }
     }).catch(function (error){
         console.log(error)
     })
