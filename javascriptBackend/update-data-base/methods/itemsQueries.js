9fdb78032460cffdd6e63602d3f8d25de1e30ca3
amauritapia <amauri.tapia@gmail.com>
2020-08-17 16:13:38 -0700

    working on getting items from open dota and formatting them

--- javascriptBackend/update-data-base/methods/itemsQueries.js
+++ javascriptBackend/update-data-base/methods/itemsQueries.js
@@ -2,36 +2,31 @@ const axios = require("axios");
 const fs = require("fs");
 
 async function getItems(){
-    return await axios.get("https://api.stratz.com/api/v1/Item")
+    return await axios.get("https://api.opendota.com/api/constants/items")
     .then(function (response){
         data = response.data;
-        /*for(n = 1; n < data.length; n++){
-            if(data[n] === undefined){
-                console.log('This is not defined. Skipping')
-            }
-            else{
-                console.log(n,data[n].language['displayName'])
-            }
-        }*/
-
-        for(i=1; i < 1032;i++){
-            if(data[i] === undefined){
-                fs.appendFile('/home/amauri-tapia/Desktop/test-2.csv',"",(err) =>{
-                    if(err) throw err;
+        
+        
+    for(name in data){
+    if(data[name].dname == undefined){
+        fs.appendFileSync('/home/amauri-tapia/Desktop/test-2.csv',data[name].id + "," + "N/A" + "," + data[name].cost + "," + data[name].img + "\r\n",(err) =>{
+            if(err) throw err;
                 })
-            } else{
-                if(data[i].language['description'] == ""){
-                    fs.appendFileSync('/home/amauri-tapia/Desktop/test-2.csv',data[i].id + "," + data[i].name + "," + "N/A" + "\r\n",(err) =>{
-                        if(err) throw err;
-                        })
-                }
-                else{
-                fs.appendFileSync('/home/amauri-tapia/Desktop/test-2.csv',data[i].id + "," + data[i].name + "," + data[i].language['description']+ "\r\n",(err) =>{
-                    if(err) throw err;
-                    })
-                }
-            }
-        }
+}   else{
+        if(data[name].cost == null){
+        fs.appendFileSync('/home/amauri-tapia/Desktop/test-2.csv',data[name].id + "," + data[name].dname + "," + "N/A" + "," + data[name].img + "\r\n",(err) =>{
+            if(err) throw err;
+                })
+}   else{
+        fs.appendFileSync('/home/amauri-tapia/Desktop/test-2.csv',data[name].id + "," + data[name].dname + "," + data[name].cost + "," + data[name].img + "\r\n",(err) =>{
+            if(err) throw err;
+                })
+}
+}
+    
+
+}
+
     }).catch(function (error){
         console.log(error)
     })
